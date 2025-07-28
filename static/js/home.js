4dde01c61a91a4e55f59a941dc59266f77b4337a
mwku <168341474+mwku@users.noreply.github.com>
2025-07-28 11:38:31 +0800

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         if (page === 0) {
             const igicon = new Image();
-            igicon.src = './static/img/igicon.jpg';
+            igicon.src = './static/img/IMG_9464.jpeg';
             igicon.alt = 'icon';
             igicon.className = 'igicon';
             igicon.onload = function () {
